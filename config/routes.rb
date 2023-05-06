b92d4f4997b2dc255c29ccee8bfd35b126857da2
Percy Manenzhe <percy@ptpi.co.za>
2023-05-06 14:49:23 +0200

    add task controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "laundry", to: "tasks#laundry"
+  get "studying", to: "tasks#studying"
 end
